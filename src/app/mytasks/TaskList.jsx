f2abd1ca4799c5b39dcd7ff90972dddd5bbff1be
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-08-16 15:50:31 +0200

    feat: add tasks layout.

---
+++ src/app/mytasks/TaskList.jsx
@@ -0,0 +1,41 @@
+function TaskList(){
+    return(
+        <>
+        <div className="flex justify-center">
+            <ul className="flex flex-col gap-2 max-w-screen-sm">
+                <li className="bg-orange-300 rounded-md p-4">
+                    <div className="flex justify-between items-center  mb-4">
+                        <h2 className="text-2xl">Task Name</h2>
+                        <button className=" bg-cyan-300 rounded-full p-2">Complete</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+                <li className="bg-orange-300 rounded-md p-4">
+                    <div className="flex justify-between items-center  mb-4">
+                        <h2 className="text-2xl">Task Name</h2>
+                        <button className=" bg-cyan-300 rounded-full p-2">Complete</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+                <li className="bg-orange-300 rounded-md p-4">
+                    <div className="flex justify-between items-center  mb-4">
+                        <h2 className="text-2xl">Task Name</h2>
+                        <button className=" bg-cyan-300 rounded-full p-2">Complete</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+                <li className="bg-orange-300 rounded-md p-4">
+                    <div className="flex justify-between items-center  mb-4">
+                        <h2 className="text-2xl">Task Name</h2>
+                        <button className=" bg-cyan-300 rounded-full p-2">Complete</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+            </ul>
+        </div>
+        
+        </>
+    )
+}
+
+export default TaskList
