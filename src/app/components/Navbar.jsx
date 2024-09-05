0dea3e8fa557fb532b82f8ccc38b53515d4cc8f4
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-09-05 18:08:46 +0200

    feat: change layout of navbar. make links active

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,26 @@
+"use client"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
+// import NavLink from 'react-router-dom'
 
 function NavBar(){
+
+    // const router = useRouter()
+    const pathname = usePathname()
     return(
         <>
-        <div className=" flex justify-between items-center p-4 border-b-2 border-black">
-            <div>
+        <div className="  items-center p-4 border-b-2 border-black">
+            {/* <div>
                 <h2 className="text-2xl"><Link href="/">MyTasks</Link></h2>
-            </div>
+            </div> */}
             <nav>
-                <ul className="flex gap-6">
-                    <li className="p-2 hover:bg-slate-300">
-                        <Link href="/mytasks">My Tasks</Link>
+                <ul className="flex gap-6 justify-center">
+                    <li className={pathname === '/mytasks' ? "p-2 bg-blue-400 rounded-md" : "hover:bg-slate-300 p-2 rounded-md"}>
+                        <Link href="/mytasks" >My Tasks</Link>
                     </li>
-                    <li className="p-2 hover:bg-slate-300">
-                        <Link href="/mytasks/create">Add Tasks</Link>
+                    <li className={pathname === '/mytasks/create' ? "p-2 bg-blue-400 rounded-md" : "hover:bg-slate-300 p-2 rounded-md"}>
+                        <Link href="/mytasks/create" >Add Tasks</Link>
                     </li>
                 </ul>
             </nav>
