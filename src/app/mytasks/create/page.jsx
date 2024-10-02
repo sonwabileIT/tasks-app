935378a16b62cf8e26dcb63bd9c83785a496aef2
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-10-02 14:01:59 +0200

    feat: adjust user interface

--- src/app/mytasks/create/page.jsx
+++ src/app/mytasks/create/page.jsx
@@ -3,7 +3,7 @@ import CreateTaskForm from "./CreateTaskForm"
 function CreateTask(){
     return(
         <>
-            <h2 className="text-4xl text-center my-7">Create Task</h2>
+            <h2 className="text-4xl text-center py-7">Create Task</h2>
             <CreateTaskForm />
         </>
     )
